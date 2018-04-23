34f7ef2c26c005c8c932e6cacaf0a553fecbf586
Will Kim <rlagmlckd@gmail.com>
2018-04-23 17:35:57 -0500

    Access-Control-Allow-Origin

--- server.js
+++ server.js
@@ -3,17 +3,29 @@ let socket = require('socket.io');
 let cors = require('cors');
 
 let app = express();
-let server = app.listen(process.env.PORT || 3000);
+
 
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+});
 app.use(express.static('public'));
 
+let server = app.listen(process.env.PORT || 3000, function() {
+
+  let host = server.address().address;
+  let port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
 
 console.log("My socket server is running");
 
 // Socket.io
 let io = socket(server);
 io.sockets.on('connection', newConnection);
+io.set('origins', '*:*');
 
 
 function newConnection(socket) {
